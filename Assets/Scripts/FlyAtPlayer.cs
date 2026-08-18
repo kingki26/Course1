c43b52fb850b0f78406b16ef3bd69bd86176e988
KienVu <kienvu26092k4@gmail.com>
2026-08-18 09:49:20 +0700

    build map and hazards

--- Assets/Scripts/FlyAtPlayer.cs
+++ Assets/Scripts/FlyAtPlayer.cs
@@ -5,6 +5,11 @@ public class FlyAtPlayer : MonoBehaviour
     [SerializeField] Transform player;
     [SerializeField] float speed = 1.0f;
     Vector3 playerPosition;
+
+    private void Awake()
+    {
+        gameObject.SetActive(false);
+    }
     void Start()
     {
         playerPosition = player.transform.position;
--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,23 +4,24 @@ public class Mover : MonoBehaviour
 {
 
     [SerializeField] float moveSpeed = 10f;
+    private Rigidbody rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
  
-    void Update()
+    void FixedUpdate()
     {
         movePlayer();
     }
 
     void movePlayer()
     {
-        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        float yValue = 0f;
-        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        float xValue = Input.GetAxis("Horizontal");
+        float zValue = Input.GetAxis("Vertical");
 
-        transform.Translate(xValue, yValue, zValue);
+        Vector3 movement = new Vector3(xValue, 0f, zValue);
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
